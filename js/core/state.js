e6dbf53980b2cc1f62fbd082c619d2b9cdbfee49
Hajjen-Game <christoffer.soderstrom@gmail.com>
2026-08-21 09:53:23 +0200

    Add game state module

---
+++ js/core/state.js
@@ -0,0 +1,14 @@
+export const state = {
+  zone: "intro",
+  x: 960,
+  y: 900,
+  dir: "up",
+  jumping: false,
+  introDone: false,
+  emberTalk: false,
+  inventory: [],
+  debug: false,
+  moving: false,
+  walkFrame: 0,
+  walkClock: 0
+};

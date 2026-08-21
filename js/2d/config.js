e21b37d0b3cb771df026adbd76b0d9c4dd935e09
Hajjen-Game <christoffer.soderstrom@gmail.com>
2026-08-21 17:04:20 +0200

    HAJJEN 2D v0.1: add world configuration

---
+++ js/2d/config.js
@@ -0,0 +1,24 @@
+export const WORLD = {
+  width: 3600,
+  height: 3600,
+  chunkSize: 600,
+  background: "#8fa77f"
+};
+
+export const VIEW = {
+  logicalWidth: 1280,
+  logicalHeight: 720,
+  cameraSmoothing: 8,
+  cullMargin: 180
+};
+
+export const PLAYER = {
+  speed: 250,
+  spawn: { x: 1800, y: 1900 },
+  radiusX: 24,
+  radiusY: 15,
+  drawWidth: 98,
+  drawHeight: 124
+};
+
+export const DEBUG_KEY = "F2";

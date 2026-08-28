384329f3f8429d0456888f38f53458c2dc4d5415
Hajjen-Game <christoffer.soderstrom@gmail.com>
2026-08-28 18:50:16 +0200

    feat(v4-b): configure Zone 2 25x10 progression

---
+++ v4-b/zone2-config.js
@@ -0,0 +1,13 @@
+window.HAJJEN_ZONE_CONFIG={
+  zone:2,name:'ZONE 2',cols:25,rows:10,viewCols:15,viewRows:10,start:{row:5,col:1},levelFloor:4,levelCap:7,next:'zone3.html',
+  introQuest:'Create a Healing Potion',introType:'potion',bossTitle:'ZONE 2 BOSS',bossPos:{row:4,col:23},
+  enemies:[
+    {row:2,col:5,type:'mob',title:'MIRE SCOUT',hp:92,attack:13,xp:20},{row:7,col:7,type:'mob',title:'BOG HUNTER',hp:98,attack:14,xp:20},
+    {row:3,col:12,type:'mob',title:'ROOT RAIDER',hp:102,attack:14,xp:21},{row:8,col:15,type:'mob',title:'MARSH HUNTER',hp:106,attack:15,xp:21},
+    {row:1,col:19,type:'mob',title:'FEN STALKER',hp:110,attack:15,xp:22},{row:6,col:21,type:'mob',title:'MIRE PROWLER',hp:112,attack:15,xp:22},
+    {row:4,col:10,type:'elite',title:'FEN GUARDIAN',hp:178,attack:19,xp:35},{row:7,col:18,type:'elite',title:'MARSH WARDEN',hp:188,attack:20,xp:38},
+    {row:4,col:23,type:'boss',title:'ZONE 2 BOSS',hp:270,attack:24,xp:80}
+  ],
+  spellIngredients:[{row:1,col:4,name:'Glowroot',force:'Growth'},{row:8,col:9,name:'Ash Pearl',force:'Ember'},{row:2,col:16,name:'Riverglass',force:'Flow'},{row:7,col:22,name:'Wind Shard',force:'Gale'}],
+  potionIngredients:[{row:4,col:6,name:'Moonleaf'},{row:5,col:17,name:'Clearwater'}]
+};

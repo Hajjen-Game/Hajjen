3722f28fa4d37f72951d32d442b4a2eabd6d05ac
Hajjen-Game <christoffer.soderstrom@gmail.com>
2026-08-28 18:50:29 +0200

    feat(v4-b): configure larger Zone 3 and Enchantment intro

---
+++ v4-b/zone3-config.js
@@ -0,0 +1,14 @@
+window.HAJJEN_ZONE_CONFIG={
+  zone:3,name:'ZONE 3',cols:30,rows:15,viewCols:15,viewRows:10,start:{row:7,col:1},levelFloor:7,levelCap:10,next:null,
+  introQuest:'Apply an Enchantment',introType:'enchantment',bossTitle:'ZONE 3 BOSS',bossPos:{row:7,col:28},
+  enemies:[
+    {row:2,col:5,type:'mob',title:'RIFT WHELP',hp:150,attack:19,xp:30},{row:11,col:7,type:'mob',title:'STONE WRAITH',hp:158,attack:20,xp:30},
+    {row:5,col:12,type:'mob',title:'GALE STALKER',hp:164,attack:20,xp:31},{row:12,col:16,type:'mob',title:'AETHER HUNTER',hp:170,attack:21,xp:31},
+    {row:2,col:22,type:'mob',title:'PRIMAL WRAITH',hp:176,attack:21,xp:32},{row:10,col:25,type:'mob',title:'RIFT PROWLER',hp:180,attack:22,xp:32},
+    {row:6,col:14,type:'elite',title:'PRIMAL SENTINEL',hp:265,attack:27,xp:50},{row:10,col:23,type:'elite',title:'RIFT GUARDIAN',hp:280,attack:28,xp:52},
+    {row:7,col:28,type:'boss',title:'ZONE 3 BOSS',hp:390,attack:32,xp:100}
+  ],
+  spellIngredients:[{row:3,col:3,name:'Thorn Core',force:'Growth'},{row:13,col:9,name:'Cinder Heart',force:'Ember'},{row:2,col:18,name:'Tide Crystal',force:'Flow'},{row:12,col:27,name:'Moon Rift',force:'Aether'}],
+  potionIngredients:[],
+  enchantment:{name:'Empowered I',damage:8,mark:'✦'}
+};

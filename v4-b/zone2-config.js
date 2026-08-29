d9b5ebb6c51a60dc3229480f1da22a425da75385
Hajjen-Game <christoffer.soderstrom@gmail.com>
2026-08-29 20:34:02 +0200

    feat(v4-b): reserve Zone 2 Primal Spring tile

--- v4-b/zone2-config.js
+++ v4-b/zone2-config.js
@@ -6,6 +6,7 @@ window.HAJJEN_ZONE_CONFIG={
     {row:3,col:12,type:'mob',title:'ROOT RAIDER',hp:102,attack:14,xp:21},{row:8,col:15,type:'mob',title:'MARSH HUNTER',hp:106,attack:15,xp:21},
     {row:1,col:19,type:'mob',title:'FEN STALKER',hp:110,attack:15,xp:22},{row:6,col:21,type:'mob',title:'MIRE PROWLER',hp:112,attack:15,xp:22},
     {row:4,col:10,type:'elite',title:'FEN GUARDIAN',hp:178,attack:19,xp:35},{row:7,col:18,type:'elite',title:'MARSH WARDEN',hp:188,attack:20,xp:38},
+    {row:4,col:17,type:'spring',title:'PRIMAL SPRING',hp:0,attack:0,xp:0},
     {row:4,col:23,type:'boss',title:'ZONE 2 BOSS',hp:270,attack:24,xp:80}
   ],
   spellIngredients:[{row:1,col:4,name:'Glowroot',force:'Growth'},{row:8,col:9,name:'Ash Pearl',force:'Ember'},{row:2,col:16,name:'Riverglass',force:'Flow'},{row:7,col:22,name:'Wind Shard',force:'Gale'}],

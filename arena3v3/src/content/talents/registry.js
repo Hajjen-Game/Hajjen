bf9f1391782982e16899d20719788bbbbf901757
Hajjen-Game <christoffer.soderstrom@gmail.com>
2026-09-24 13:15:13 +0200

    Register Death Knight talent tree

--- arena3v3/src/content/talents/registry.js
+++ arena3v3/src/content/talents/registry.js
@@ -2,10 +2,12 @@ import { priestTalentTree } from "../classes/priest/talents.js";
 import { warriorTalentTree } from "../classes/warrior/talents.js";
 import { rogueTalentTree } from "../classes/rogue/talents.js";
 import { shamanTalentTree } from "../classes/shaman/talents.js";
+import { deathKnightTalentTree } from "../classes/death-knight/talents.js";
 
 export const TALENT_TREE_REGISTRY = Object.freeze({
   priest: priestTalentTree,
   warrior: warriorTalentTree,
   rogue: rogueTalentTree,
   shaman: shamanTalentTree,
+  "death-knight": deathKnightTalentTree,
 });

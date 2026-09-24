938368e22d0c7ca373047d8f7977204d56402fae
Hajjen-Game <christoffer.soderstrom@gmail.com>
2026-09-24 13:08:33 +0200

    Register Shaman talent tree

--- arena3v3/src/content/talents/registry.js
+++ arena3v3/src/content/talents/registry.js
@@ -1,9 +1,11 @@
 import { priestTalentTree } from "../classes/priest/talents.js";
 import { warriorTalentTree } from "../classes/warrior/talents.js";
 import { rogueTalentTree } from "../classes/rogue/talents.js";
+import { shamanTalentTree } from "../classes/shaman/talents.js";
 
 export const TALENT_TREE_REGISTRY = Object.freeze({
   priest: priestTalentTree,
   warrior: warriorTalentTree,
   rogue: rogueTalentTree,
+  shaman: shamanTalentTree,
 });

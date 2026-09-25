797d93b9fa6c6a42287e7dd566dfc70ab33bbcbb
Hajjen-Game <christoffer.soderstrom@gmail.com>
2026-09-25 21:03:58 +0200

    Refresh caster school talent data

--- arena3v3/src/content/talents/registry.js
+++ arena3v3/src/content/talents/registry.js
@@ -1,4 +1,4 @@
-import { priestTalentTree } from "../classes/priest/talents.js";
+import { priestTalentTree } from "../classes/priest/talents.js?v=20260925-schools1";
 import { druidTalentTree } from "../classes/druid/talents.js";
 import { mageTalentTree } from "../classes/mage/talents.js";
 import { paladinTalentTree } from "../classes/paladin/talents.js";
@@ -6,7 +6,7 @@ import { warriorTalentTree } from "../classes/warrior/talents.js";
 import { rogueTalentTree } from "../classes/rogue/talents.js";
 import { shamanTalentTree } from "../classes/shaman/talents.js";
 import { deathKnightTalentTree } from "../classes/death-knight/talents.js";
-import { warlockTalentTree } from "../classes/warlock/talents.js";
+import { warlockTalentTree } from "../classes/warlock/talents.js?v=20260925-schools1";
 
 export const TALENT_TREE_REGISTRY = Object.freeze({
   priest: priestTalentTree,

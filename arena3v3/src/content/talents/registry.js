dc5193664c6185e9a288f0f1dab3f049a58826b3
Hajjen-Game <christoffer.soderstrom@gmail.com>
2026-09-25 08:44:17 +0200

    Register Mage talent tree

--- arena3v3/src/content/talents/registry.js
+++ arena3v3/src/content/talents/registry.js
@@ -1,5 +1,6 @@
 import { priestTalentTree } from "../classes/priest/talents.js";
 import { druidTalentTree } from "../classes/druid/talents.js";
+import { mageTalentTree } from "../classes/mage/talents.js";
 import { warriorTalentTree } from "../classes/warrior/talents.js";
 import { rogueTalentTree } from "../classes/rogue/talents.js";
 import { shamanTalentTree } from "../classes/shaman/talents.js";
@@ -9,6 +10,7 @@ import { warlockTalentTree } from "../classes/warlock/talents.js";
 export const TALENT_TREE_REGISTRY = Object.freeze({
   priest: priestTalentTree,
   druid: druidTalentTree,
+  mage: mageTalentTree,
   warrior: warriorTalentTree,
   rogue: rogueTalentTree,
   shaman: shamanTalentTree,

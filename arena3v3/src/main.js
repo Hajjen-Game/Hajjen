50e38c55f0ff64871f136568e81552243b5a8d88
Hajjen-Game <christoffer.soderstrom@gmail.com>
2026-09-26 20:34:56 +0200

    Refresh AI gear parity build

--- arena3v3/src/main.js
+++ arena3v3/src/main.js
@@ -1,4 +1,4 @@
-import { Game } from "./core/Game.js?v=20260926-revertstamina1";
+import { Game } from "./core/Game.js?v=20260926-aigear1";
 import { InputManager } from "./core/InputManager.js?v=20260925-keycapture1";
 import { CharacterStore } from "./core/CharacterStore.js";
 import { HonorSystem, legacyHonorAvailable, migrateLegacyHonor } from "./core/HonorSystem.js";

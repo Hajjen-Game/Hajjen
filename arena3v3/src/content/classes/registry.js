905f716f262e50d268d3812494f32d6f84f6d3f1
Hajjen-Game <christoffer.soderstrom@gmail.com>
2026-09-26 13:10:34 +0200

    Refresh healer mana rebalance configs

--- arena3v3/src/content/classes/registry.js
+++ arena3v3/src/content/classes/registry.js
@@ -1,6 +1,6 @@
-import { priestClass } from "./priest/config.js?v=20260926-manapressure1";
-import { druidClass } from "./druid/config.js?v=20260926-manapressure1";
-import { paladinClass } from "./paladin/config.js?v=20260926-manapressure1";
+import { priestClass } from "./priest/config.js?v=20260926-manapressure2";
+import { druidClass } from "./druid/config.js?v=20260926-manapressure2";
+import { paladinClass } from "./paladin/config.js?v=20260926-manapressure2";
 import { warriorClass } from "./warrior/config.js?v=20260926-slamai1";
 import { rogueClass } from "./rogue/config.js";
 import { deathKnightClass } from "./death-knight/config.js";

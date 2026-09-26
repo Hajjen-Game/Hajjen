9f27c9e2f30a99d268d7ecc1f058171d1f50e04f
Hajjen-Game <christoffer.soderstrom@gmail.com>
2026-09-26 09:49:39 +0200

    Refresh talent registry for Mind Blast VFX

--- arena3v3/src/core/TalentSystem.js
+++ arena3v3/src/core/TalentSystem.js
@@ -1,4 +1,4 @@
-import { TALENT_TREE_REGISTRY } from "../content/talents/registry.js?v=20260925-schools1";
+import { TALENT_TREE_REGISTRY } from "../content/talents/registry.js?v=20260926-mindblastvfx1";
 
 const STORAGE_PREFIX = "arena3v3-talents-v1:";
 
